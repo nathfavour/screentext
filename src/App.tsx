ee87615a2071424460ab15e33881ad326b6c463c
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-12-15 16:43:10 +0100

    fix: update tauri configuration for window transparency and decorations

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import React from "react";
 function App() {
   return (
     <main
+      data-tauri-drag-region
       className="container"
       style={{ WebkitAppRegion: "drag", width: "100vw", height: "100vh" } as React.CSSProperties}
     >
