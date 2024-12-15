f6b61515e97ae6ae1cbefa99936f0cb3ad9343b8
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-12-15 15:28:15 +0100

    fix: add React import and specify CSS properties in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 
+import React from "react";
+
+  
+
 function App() {
   return (
     <main
       className="container"
-      style={{ WebkitAppRegion: "drag", width: "100vw", height: "100vh" }}
+      style={{ WebkitAppRegion: "drag", width: "100vw", height: "100vh" } as React.CSSProperties}
     >
       {/* Blank content */}
     </main>
